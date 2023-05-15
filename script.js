06f20f0b55a8bc93123898372147ce8ba32f4a54
OliverLFJ <oliverde10021@gmail.com>
2023-05-15 02:05:30 -0600

    Pushing project into the repository

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("Entro a la funcion")
+    const eyeLeft = document.getElementById('iris-container');
+
+    const open = document.getElementById('open-mouth');
+    const close = document.getElementById('close-mouth');
+
+
+    setInterval(() => {
+        eyeLeft.classList.add('iris-left-animate');
+        setTimeout(() => {
+            eyeLeft.classList.remove('iris-left-animate');
+        }, 4000);
+    }, 3000);
+
+    setTimeout(() => {
+        setInterval(() => {
+            eyeLeft.classList.add('move-left-animate');
+            setTimeout(() => {
+                eyeLeft.classList.remove('move-left-animate');
+            }, 6000);
+        }, 10000);
+    }, 4000);
+
+    setInterval(() => {
+        open.style.display = "none"
+        close.style.display = "inline-flex"
+        setTimeout(() => {
+            open.style.display = "inline-flex"
+            close.style.display = "none"
+        }, 2000);
+    }, 10000);
+
+})
